d43deb8aed86e08ef0c7d3720ec4bddb9d8a1648
lala <cloudlala0@gmail.com>
2025-03-24 16:15:11 +0900

    프로젝트 완료

--- front/iris.js
+++ front/iris.js
@@ -1,28 +1,67 @@
+ google.charts.load('current', {'packages':['corechart']});
+
+      function drawChart(prob, pid) {
+        var data = google.visualization.arrayToDataTable([
+          ['iris species', 'prob'],
+          ['setosa',     prob[0][0]],
+          ['versicolor',      prob[0][1]],
+          ['virginica',  prob[0][2]]
+        ]);
+
+        var options = {
+          title: '붓꽃 품종 확률',
+          //is3D: true,
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(pid);
+        chart.draw(data, options);
+      }
+
+
+function drawCharto(prob, pid){
+  var data = google.visualization.arrayToDataTable([
+    ['iris species', 'prob'],
+    ['setosa',     prob[0][0]],
+    ['versicolor',  prob[0][1]],
+    ['virginica', prob[0][2]]
+  ]);
+
+  var options = {
+    title: 'probability of classification'
+  };
+
+  var chart = new google.visualization.PieChart(pid);
+  chart.draw(data, options);
+}
 function Send(){
     sl = document.getElementById("sl")
     sw = document.getElementById("sw")
     pl = document.getElementById("pl")
     pw = document.getElementById("pw")
 
-
     var data = {
-        'sepal_length':sl.value,
-        'sepal_width':sw.value,
-        'petal_length':pl.value,
-        'petal_width':pw.value
+        "sepal_length": sl.value,
+        "sepal_width": sw.value,
+        "petal_length": pl.value,
+        "petal_width": pw.value
     }
+
+
     $.ajax({
-        type: "Post",
-        url: 'http://localhost:8000/predict',
-        headers: {
+        type: "POST",
+        url: "http://localhost:8000/predict",
+        headers:{
             "Accept": "application/json",
             "Content-Type": "application/json",
         },
-        data: JSON.stringify(data)
-    }).done(function(response){
-        console.log(response)
-        txtOut.value = response.prediction + " 일 확률: " + response.probability
-    }).fail(function(response){
-        alert("fail"+response)
-    }).always()
+        data: JSON.stringify(data),
+       }).done(function(response){
+            console.log(response)
+            txtOut.value = response.prediction + " 일 확률: " + response.probability
+            google.charts.setOnLoadCallback(drawChart(response.probability, document.getElementById("piechart") ));
+       }).fail(function(response){
+            alert("fail" + response)
+       }
+       ).always()
 }
